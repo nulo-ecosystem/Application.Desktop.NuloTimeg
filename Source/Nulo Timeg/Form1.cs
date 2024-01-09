1b2c03ecc9961cd52370553c23c70caa784cab59
Kastney Frazão <kastney@outlook.com>
2024-01-09 17:10:00 -0300

    🔧 chore - Update metadata.

--- Source/Nulo Timeg/Form1.cs
+++
@@ -1,7 +0,0 @@
-namespace Nulo_Timeg {
-    public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-        }
-    }
-}
--- Source/Nulo Timeg/Program.cs
+++ Source/Nulo Timeg/Program.cs
@@ -1,4 +1,4 @@
-namespace Nulo_Timeg {
+namespace Nulo {
     internal static class Program {
         /// <summary>
         ///  The main entry point for the application.
@@ -8,7 +8,7 @@ namespace Nulo_Timeg {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
